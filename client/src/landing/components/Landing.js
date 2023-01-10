6cff6448ca6268521b01ed7a25ec9ac5227602bc
bryanzack <bryan.zack@zacklabs.com>
2023-01-10 11:43:20 -0500

    started mouseHover gradient animation

--- client/src/landing/components/Landing.js
+++ client/src/landing/components/Landing.js
@@ -2,32 +2,74 @@
 exports.__esModule = true;
 var React = require('react');
 require("./Landing.css");
+var react_1 = require("react");
+var fiber_1 = require("@react-three/fiber");
+var web_1 = require("@react-spring/web");
+var TestBox = function () {
+    var myMesh = (0, react_1.useRef)(null);
+    (0, fiber_1.useFrame)(function (_a) {
+        var clock = _a.clock;
+        myMesh.current.rotation.x = Math.sin(clock.getElapsedTime());
+    });
+    return (React.createElement("mesh", { ref: myMesh, onClick: function (e) { return console.log("Test!"); }, onPointerMissed: function () { return console.log("miss!"); } },
+        React.createElement("boxGeometry", null),
+        React.createElement("meshBasicMaterial", { color: 'royalblue' })));
+};
 var Line = function (props) {
-    var evenStyle = {
-    //color: "red",
-    //marginLeft: '20px',
-    };
-    var oddStyle = {};
     var arr = [];
     for (var i = 0; i < 30; i++) {
         arr.push(i);
     }
-    return (React.createElement("div", { className: "line", style: props.num % 2 === 0 ? evenStyle : {} }, arr.map(function (index) {
+    return (React.createElement("div", { className: "line" }, arr.map(function (index) {
         return React.createElement("p", { key: index }, "bryan zack \u00A0");
     })));
 };
-var Landing = function () {
+var Names = function () {
+    var _a = (0, react_1.useState)(false), mouseHover = _a[0], setMouseHover = _a[1];
     var arr = [];
     for (var i = 0; i < 40; i++) {
         arr.push(i);
     }
+    var active = {
+        opacity: 1,
+        transition: 'opacity 500ms'
+    };
+    var inactive = {
+        opacity: 0,
+        transition: 'opacity 500ms'
+    };
     return (React.createElement(React.Fragment, null,
-        React.createElement("div", { className: "landing" },
+        React.createElement("div", { className: 'names' }, arr.map(function (index) {
+            return React.createElement(Line, { num: index, key: index });
+        })),
+        React.createElement("div", { style: mouseHover ? active : inactive, className: "background", onMouseEnter: function () { return setMouseHover(true); }, onMouseLeave: function () { return setMouseHover(false); } }, "test")));
+};
+var Landing = function () {
+    var fadeIn = (0, web_1.useTransition)(null, {
+        from: { opacity: 0 },
+        enter: { opacity: 1 },
+        leave: { opacity: 0 }
+    });
+    var backgroundFade = (0, web_1.useSpring)({
+        from: {
+            background: "linear-gradient(217deg, rgba(255,0,0,.8), rgba(255,0,0,0) 70.71%),\n                             linear-gradient(127deg, rgba(0,255,0,.8), rgba(0,255,0,0) 70.71%),\n                             linear-gradient(336deg, rgba(0,0,255,.8), rgba(0,0,255,0) 70.71%)"
+        },
+        to: {
+            background: 'none'
+        }
+    });
+    var hover = {
+        background: "linear-gradient(217deg, rgba(255,0,0,.8), rgba(255,0,0,0) 70.71%),\n    linear-gradient(127deg, rgba(0,255,0,.8), rgba(0,255,0,0) 70.71%),\n    linear-gradient(336deg, rgba(0,0,255,.8), rgba(0,0,255,0) 70.71%)"
+    };
+    var not_hover = {
+        background: 'white',
+        transition: 'background .5s ease'
+    };
+    return fadeIn(function (style) { return (React.createElement(React.Fragment, null,
+        React.createElement(web_1.animated.div, { style: style, className: "landing" },
             React.createElement("div", { className: "sidewaystext" },
                 React.createElement("h1", null, "bryan zack")),
-            React.createElement("div", { className: "names" }, arr.map(function (index) {
-                return React.createElement(Line, { num: index, key: index });
-            })))));
+            React.createElement(Names, null)))); });
 };
 exports["default"] = Landing;
 /*
--- client/src/landing/components/Landing.tsx
+++ client/src/landing/components/Landing.tsx
@@ -1,52 +1,112 @@
 const React = require('react');
 import './Landing.css';
 
-import { FC, ReactElement, useRef } from 'react';
+import { FC, ReactElement, useRef, useState } from 'react';
 import type { RootState } from '../../store';
-import { Canvas } from  '@react-three/fiber';
+import { Canvas, useFrame } from  '@react-three/fiber';
+import { Mesh } from 'three';
+import { animated, useTransition, useSpring } from '@react-spring/web';
 
 interface LineProps {
     num: number,
 }
-const Line: FC<LineProps> = (props): ReactElement => {
-    const evenStyle = {
-        //color: "red",
-        //marginLeft: '20px',
-    }
-    const oddStyle = {
 
-    }
+const TestBox: FC = (): ReactElement => {
+    const myMesh = useRef<Mesh>(null!);
+    useFrame(({ clock }) => {
+        myMesh.current.rotation.x = Math.sin(clock.getElapsedTime());
+    });
+    return (
+        <mesh
+            ref={myMesh}
+            onClick={(e) => console.log("Test!")}
+            onPointerMissed={() => console.log("miss!")}>
+            <boxGeometry />
+            <meshBasicMaterial color={'royalblue'} />
+        </mesh>
+    );
+}
+const Line: FC<LineProps> = (props): ReactElement => {
     const arr: number[] = [];
     for (let i = 0; i < 30; i++) {
         arr.push(i);
     }
     return (
-        <div className="line" style={props.num % 2 === 0 ? evenStyle : {}}>
+        <div className="line">
             {arr.map((index: number) => {
                 return <p key={index}>bryan zack &nbsp;</p>
             })}
         </div>
     );
 }
-const Landing: FC = (): ReactElement => {
+
+const Names: FC = (): ReactElement => {
+    const [mouseHover, setMouseHover] = useState(false);
     const arr: number[] = [];
     for (let i = 0; i < 40; i++) {
         arr.push(i);
     }
+
+    const active = {
+        opacity: 1,
+        transition: 'opacity 500ms',
+    }
+    const inactive = {
+        opacity: 0,
+        transition: 'opacity 500ms',
+    }
+
     return (
         <>
-            <div className="landing">
-                <div className="sidewaystext">
-                    <h1>bryan zack</h1>
-                </div>
-                <div className="names">
-                    {arr.map((index: number) => {
-                        return <Line num={index} key={index}/>
-                    })}
-                </div>
+            <div className={'names'}>
+                {arr.map((index: number) => {
+                    return <Line num={index} key={index}/>
+                })}
+            </div>
+            <div style={mouseHover ? active : inactive} className="background" onMouseEnter={() => setMouseHover(true)} onMouseLeave={() => setMouseHover(false)}>
+                test
             </div>
         </>
     );
+}
+const Landing: FC = (): ReactElement => {
+    const fadeIn = useTransition(null, {
+        from: { opacity: 0 },
+        enter: { opacity: 1 },
+        leave: { opacity: 0 },
+    });
+
+    const backgroundFade = useSpring({
+        from: {
+            background: `linear-gradient(217deg, rgba(255,0,0,.8), rgba(255,0,0,0) 70.71%),
+                             linear-gradient(127deg, rgba(0,255,0,.8), rgba(0,255,0,0) 70.71%),
+                             linear-gradient(336deg, rgba(0,0,255,.8), rgba(0,0,255,0) 70.71%)`,
+        },
+        to: {
+            background: 'none',
+        }
+    })
+    const hover = {
+        background: `linear-gradient(217deg, rgba(255,0,0,.8), rgba(255,0,0,0) 70.71%),
+    linear-gradient(127deg, rgba(0,255,0,.8), rgba(0,255,0,0) 70.71%),
+    linear-gradient(336deg, rgba(0,0,255,.8), rgba(0,0,255,0) 70.71%)`,
+    }
+    const not_hover = {
+        background: 'white',
+        transition: 'background .5s ease',
+
+    }
+
+    return fadeIn((style) => (
+        <>
+            <animated.div style={style} className="landing">
+                <div className="sidewaystext">
+                    <h1>bryan zack</h1>
+                </div>
+                <Names />
+            </animated.div>
+        </>
+    ));
 }
 
 export default Landing;
