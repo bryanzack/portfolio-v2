766b61b999b1abc9dbd18563ca62dff0513e9ffb
bryan zack <bryan.zack@zacklabs.com>
2023-01-13 17:32:28 -0500

    added inverted CursorAnimation

--- client/src/landing/components/CursorAnimation.js
+++ client/src/landing/components/CursorAnimation.js
@@ -6,6 +6,9 @@ var web_1 = require("@react-spring/web");
 var react_use_measure_1 = require("react-use-measure");
 var fast = { tension: 1200, friction: 40 };
 var slow = { mass: 10, tension: 200, friction: 50 };
+var transInverted = function (x, y) {
+    return "translate3d(".concat(window.innerWidth - x, "px,").concat(window.innerHeight - y, "px,0) translate3d(-50%,-50%,0)");
+};
 var trans = function (x, y) {
     return "translate3d(".concat(x, "px,").concat(y, "px,0) translate3d(-50%,-50%,0)");
 };
@@ -23,6 +26,7 @@ var CursorAnimation = function () {
             React.createElement("filter", { id: "goo" },
                 React.createElement("feGaussianBlur", { "in": "SourceGraphic", result: "blur", stdDeviation: "30" }),
                 React.createElement("feColorMatrix", { "in": "blur", values: "1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 30 -7" }))),
-        React.createElement("div", { ref: ref, className: 'cursor-animation-main', onMouseMove: handleMouseMove }, trail.map(function (props, index) { return (React.createElement(web_1.animated.div, { key: index, style: { transform: props.xy.to(trans) } })); }))));
+        React.createElement("div", { ref: ref, className: 'cursor-animation-main', onMouseMove: handleMouseMove }, trail.map(function (props, index) { return (React.createElement(web_1.animated.div, { key: index, style: { transform: props.xy.to(trans) } })); })),
+        React.createElement("div", { ref: ref, className: 'cursor-animation-main', onMouseMove: handleMouseMove }, trail.map(function (props, index) { return (React.createElement(web_1.animated.div, { key: index, style: { transform: props.xy.to(transInverted) } })); }))));
 };
 exports["default"] = CursorAnimation;
--- client/src/landing/components/CursorAnimation.tsx
+++ client/src/landing/components/CursorAnimation.tsx
@@ -7,6 +7,8 @@ import useMeasure from 'react-use-measure';
 
 const fast = { tension: 1200, friction: 40 };
 const slow = {mass: 10, tension: 200, friction: 50 };
+const transInverted = (x: number, y: number) =>
+    `translate3d(${window.innerWidth-x}px,${window.innerHeight-y}px,0) translate3d(-50%,-50%,0)`;
 const trans = (x: number, y: number) =>
     `translate3d(${x}px,${y}px,0) translate3d(-50%,-50%,0)`;
 
@@ -35,6 +37,11 @@ const CursorAnimation = () => {
             <div ref={ref} className={'cursor-animation-main'} onMouseMove={handleMouseMove}>
                 {trail.map((props, index) => (
                     <animated.div key={index} style={{ transform: props.xy.to(trans) }} />
+                ))}
+            </div>
+            <div ref={ref} className={'cursor-animation-main'} onMouseMove={handleMouseMove}>
+                {trail.map((props, index) => (
+                    <animated.div key={index} style={{ transform: props.xy.to(transInverted) }} />
                 ))}
             </div>
         </div>
