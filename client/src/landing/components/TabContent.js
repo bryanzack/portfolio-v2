04f608db6cf217293805568a8a3e8f003414bd0b
bryanzack <bryan.zack@zacklabs.com>
2023-01-13 12:15:24 -0500

    sucessfully implemented mapping of component children

--- client/src/landing/components/TabContent.js
+++ client/src/landing/components/TabContent.js
@@ -3,9 +3,10 @@ exports.__esModule = true;
 var React = require('react');
 require("./TabContent.css");
 var react_redux_1 = require("react-redux");
-var About = function () {
-    return (React.createElement("div", { className: "tab-content" },
-        React.createElement("h1", null, "PENN STATE BEHREND")));
+var About = function (_a) {
+    var children = _a.children;
+    var items = React.Children.toArray(children);
+    return (React.createElement("div", { className: "tab-content" }, items.map(function (item, index) { return (React.createElement("div", { key: index }, item)); })));
 };
 var Tech = function () {
     return (React.createElement("div", { className: "tab-content" },
@@ -19,7 +20,10 @@ var TabContent = function () {
     var active_tab = (0, react_redux_1.useSelector)(function (state) { return state.nav.active_tab; });
     switch (active_tab) {
         case 'about':
-            return (React.createElement(About, null));
+            return (React.createElement(About, null,
+                React.createElement("h1", null, "PENN STATE"),
+                React.createElement("h1", null, "MUSIC ENTHUSIAST"),
+                React.createElement("h1", null, "WEB DEVELOPER")));
         case 'tech':
             return (React.createElement(Tech, null));
         case 'projects':
--- client/src/landing/components/TabContent.tsx
+++ client/src/landing/components/TabContent.tsx
@@ -1,14 +1,24 @@
+import {isContentEditable} from "@testing-library/user-event/dist/utils";
+
 const React = require('react');
 import './TabContent.css';
 
-import { FC, ReactElement } from 'react';
+import {FC, ReactElement, ReactNode} from 'react';
 import type { RootState } from "../../store";
 import { useSelector } from 'react-redux';
+import { animated, useSpring } from '@react-spring/web';
 
-const About: FC = (): ReactElement => {
+interface ContentProps {
+    children?: React.ReactNode;
+}
+
+const About: FC<ContentProps> = ({ children }): ReactElement => {
+    const items = React.Children.toArray(children);
     return (
         <div className={"tab-content"}>
-            <h1>PENN STATE BEHREND</h1>
+            {items.map((item: ReactNode, index: number) => (
+                <div key={index}>{item}</div>
+            ))}
         </div>
     );
 }
@@ -31,7 +41,13 @@ const TabContent: FC = (): ReactElement => {
     const active_tab = useSelector((state: RootState) => state.nav.active_tab);
     switch (active_tab) {
         case 'about':
-            return (<About />);
+            return (
+                <About>
+                        <h1>PENN STATE</h1>
+                        <h1>MUSIC ENTHUSIAST</h1>
+                        <h1>WEB DEVELOPER</h1>
+                </About>
+            );
         case 'tech':
             return (<Tech />);
         case 'projects':
